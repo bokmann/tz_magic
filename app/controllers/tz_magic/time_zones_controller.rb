9a0187d7bbe8d0e9553ef151272dd72fc7ee5b48
bokmann <dbock@codesherpas.com>
2012-10-15 14:48:54 -0400

    making sure our before_filter isn't accidentally applied to our controller

--- app/controllers/tz_magic/time_zones_controller.rb
+++ app/controllers/tz_magic/time_zones_controller.rb
@@ -1,6 +1,8 @@
 module TzMagic
   class TimeZonesController < ApplicationController
 
+    skip_before_filter :ensure_timezone
+
     def create
       session[:timezone] = params[:timezone_name]
       forward_to = session[:timezone_return_to] || root_path
